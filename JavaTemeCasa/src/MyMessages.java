372d65a15458121a6485877c5e6944aacc027a76
mszuchan <mszuchan@MSZUCHAN-RO.ro.oracle.com>
2016-08-10 23:20:50 +0300

    Prima Salvare

---
+++ JavaTemeCasa/src/MyMessages.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author mszuchan
+ */
+public class MyMessages {
+public static void main(String[] args) {
+System.out.println ("This is my first Java Program");
+System.out.println ("So, hello Java it is !");
+System.out.println ("Java is WORA!");
+}
+}
