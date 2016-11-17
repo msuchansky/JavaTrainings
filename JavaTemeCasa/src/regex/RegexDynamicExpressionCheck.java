cb307660a28a9763d7e10420f0e7808d0b4f73cb
Miro <Miro@DESKTOP-6V2HNR3>
2016-11-17 21:42:43 +0200

    Completed pattern searched in source

---
+++ JavaTemeCasa/src/regex/RegexDynamicExpressionCheck.java
@@ -0,0 +1,47 @@
+/**
+ * package contains identifer of a string in a string 
+ */
+package regex;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * class RegexDynamicExpressionCheck has a main method that identifies and 
+ * displays a pattern introduced by the user into a source string introduced 
+ * also by the user   
+ * @author miroslavzsuchanszki
+ */
+public class RegexDynamicExpressionCheck {
+    
+    public static void main(String[] args){
+        Scanner scannerRegex = new Scanner(System.in);
+        System.out.println("Insert regex or pattern to be searched: ");
+        // read the regular expresion
+        String regex = scannerRegex.nextLine();
+        // create a pattern using introduced regular expresion
+        Pattern pattern = Pattern.compile(regex);
+        // read the source string using a new Scanner object
+        Scanner scannerSourceString = new Scanner(System.in);
+        System.out.println("Insert source string: ");
+        String source = scannerSourceString.nextLine();
+        // create the matcher as result of applying regex on source string
+        Matcher matcher = pattern.matcher(source);
+        boolean found = false;
+        // if the regular expression or pattern was found
+        while (matcher.find()) {
+            String result;
+            result = String.format("I found text \"%s\" starting at " + 
+                    "index %d until index %d.",
+                    matcher.group(),
+                    matcher.start(),
+                    matcher.end());
+            System.out.println(result);
+            found = true;
+        }
+        // if regular expression or pattern was not found in source string
+        if (!found) {
+            System.out.println("No match found :(");
+        }
+        }
+    }
